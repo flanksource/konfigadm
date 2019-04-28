4c55bacaa89ca9cdb9da0909376cddbe5f993cce
Moshe Immerman <moshe.immerman@vitalitygroup.com>
2019-04-28 21:11:51 +0200

    feat: add packages tests

--- pkg/phases/commands_test.go
+++ pkg/phases/commands_test.go
@@ -1,6 +1,9 @@
 package phases
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestCommandRuntimeFlag(t *testing.T) {
 }
@@ -10,4 +13,17 @@ func TestPreCommand(t *testing.T) {
 func TestPostCommand(t *testing.T) {
 }
 func TestCommand(t *testing.T) {
+}
+func TestCommandInterpolation(t *testing.T) {
+}
+
+func (cfg *SystemConfig) FindCmd(prefix string) []*Command {
+	cmds := []*Command{}
+
+	for _, cmd := range cfg.PreCommands {
+		if strings.HasPrefix(cmd.Cmd, prefix) {
+			cmds = append(cmds, &cmd)
+		}
+	}
+	return cmds
 }
