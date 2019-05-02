f3530c87582eed696f6854a1cde930a027c0267c
Moshe Immerman <moshe.immerman@vitalitygroup.com>
2019-05-02 12:22:59 +0200

    feat: goreleaser

---
+++ scripts/release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+NAME=configadm
+GITHUB_USER=moshloop
+TAG := $(shell git tag --points-at HEAD )
+
+go get github.com/goreleaser/goreleaser
+goreleaser release --rm-dist
+
+go get github.com/aktau/github-release
+github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME} -f dist/linux_amd64/${NAME}
+github-release upload -u $GITHUB_USER - -r ${NAME} --tag $TAG -n ${NAME}_osx -f dist/darwin_amd64/${NAME}
+github-release upload -u $GITHUB_USER - -r ${NAME} --tag $TAG -n ${NAME}.exe -f dist/windows_amd64/${NAME}.exe
