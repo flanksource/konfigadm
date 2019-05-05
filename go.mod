79b075c2247ceb2088f12a53f35654baca8602db
Moshe Immerman <moshe.immerman@vitalitygroup.com>
2019-05-05 11:31:49 +0200

    add integration test

--- go.mod
+++ go.mod
@@ -1,13 +1,23 @@
 module github.com/moshloop/configadm
 
 require (
+	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
 	github.com/aktau/github-release v0.7.2 // indirect
+	github.com/cenkalti/backoff v2.1.1+incompatible // indirect
+	github.com/codeskyblue/go-sh v0.0.0-20190412065543-76bd3d59ff27
+	github.com/containerd/continuity v0.0.0-20190426062206-aaeac12a7ffc // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/flosch/pongo2 v0.0.0-20181225140029-79872a7b2769
 	github.com/goreleaser/goreleaser v0.106.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/juju/errors v0.0.0-20190207033735-e65537c515d7 // indirect
 	github.com/onsi/gomega v1.5.0
+	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/opencontainers/runc v0.1.1 // indirect
+	github.com/ory/dockertest v3.3.4+incompatible
 	github.com/sirupsen/logrus v1.4.1
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3 // indirect
---
+++ test/setup_test.go
@@ -0,0 +1,101 @@
+package test
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
+	"github.com/ory/dockertest"
+)
+
+var binary = "dist/linux_amd64/configadm"
+
+// uses a sensible default on windows (tcp/http) and linux/osx (socket)
+var docker, _ = dockertest.NewPool("")
+
+type Container struct {
+	id        string
+	container *dockertest.Resource
+}
+
+func (c *Container) Delete() {
+	c.container.Close()
+}
+
+func (c *Container) Exec(args ...string) (string, error) {
+	arg := []string{"exec", c.id}
+	arg = append(arg, args...)
+	cmd := exec.Command("docker", arg...)
+	data, err := cmd.CombinedOutput()
+	if err != nil {
+		return string(data), err
+	}
+	cmd.Wait()
+	if !cmd.ProcessState.Success() {
+		return string(data), errors.New("Failed")
+	}
+	return string(data), nil
+}
+
+//newContainer creates a new systemd based container and returns the container id
+func newContainer() (*Container, error) {
+	env := os.Environ()
+	cwd, _ := os.Getwd()
+	cwd = filepath.Dir(cwd)
+	binary = cwd + "/" + binary
+	volumes := []string{
+		fmt.Sprintf("%s:%s", cwd, cwd),
+		"/sys/fs/cgroup:/sys/fs/cgroup",
+	}
+
+	opts := dockertest.RunOptions{
+		Privileged: true,
+		Env:        env,
+		Repository: "moshloop/docker-ubuntu1804-ansible",
+		Entrypoint: []string{"/lib/systemd/systemd"},
+		Mounts:     volumes,
+	}
+
+	container, err := docker.RunWithOptions(&opts)
+	if err != nil {
+		return nil, err
+	}
+	return &Container{
+		id:        container.Container.ID,
+		container: container,
+	}, nil
+}
+
+func check(t *testing.T, err error) {
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func setup(t *testing.T) (*gomega.WithT, *Container) {
+	g := gomega.NewGomegaWithT(t)
+	container, err := newContainer()
+	if err != nil {
+		t.Fatal(err)
+	}
+	return g, container
+}
+
+func TestVersion(t *testing.T) {
+	g, container := setup(t)
+	defer container.Delete()
+
+	stdout, err := container.Exec(binary, "version")
+	fmt.Println(stdout)
+	if err != nil {
+		t.Fatalf("%s: %s", err, stdout)
+	}
+	g.Expect(stdout).To(ContainSubstring("built at"))
+	g.Expect(strings.Split(stdout, "\n")).To(HaveLen(2))
+}
