ad9099f5ae88f7f36e6986917cc71c5bae2ec569
Moshe Immerman <moshe.immerman@gmail.com>
2019-05-12 14:49:00 +0200

    feat: add file/template verification

--- pkg/phases/files.go
+++ pkg/phases/files.go
@@ -23,6 +23,30 @@ func (p filesPhase) ApplyPhase(sys *Config, ctx *SystemContext) ([]Command, File
 	return commands, files, nil
 }
 
+func (f filesPhase) Verify(cfg *Config, results *VerifyResults, flags ...Flag) bool {
+	verify := true
+	for f := range cfg.Files {
+
+		if _, err := os.Stat(f); err != nil {
+			verify = false
+			results.Fail("%s does not exist", f)
+		} else {
+			results.Pass("%s exists", f)
+		}
+	}
+
+	for f := range cfg.Templates {
+		if _, err := os.Stat(f); err != nil {
+			verify = false
+			results.Fail("%s does not exist", f)
+		} else {
+			results.Pass("%s exists", f)
+		}
+	}
+
+	return verify
+}
+
 func Lookup(path string) string {
 	_, err := os.Stat(path)
 	if err != nil {
