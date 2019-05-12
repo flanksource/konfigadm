ac4eef670592634e9cbc2d3b0d9f122f1810e2e1
Moshe Immerman <moshe.immerman@gmail.com>
2019-05-12 19:03:28 +0200

    fix unit tests

--- pkg/phases/packages_test.go
+++ pkg/phases/packages_test.go
@@ -12,14 +12,14 @@ func TestPackageRuntimeFlag(t *testing.T) {
 
 func TestPackageDebian(t *testing.T) {
 	cfg, g := NewFixture("packages.yml", t).WithFlags(DEBIAN).Build()
-	g.Expect(cfg).To(ContainPackage("netcat"))
+	g.Expect(cfg).To(ContainPackage("netcat-openbsd"))
 	g.Expect(cfg).NotTo(ContainPackage("nmap-netcat"))
 
 }
 
 func TestPackageRedhat(t *testing.T) {
 	cfg, g := NewFixture("packages.yml", t).WithFlags(REDHAT).Build()
-	g.Expect(cfg).NotTo(ContainPackage("netcat"))
+	g.Expect(cfg).NotTo(ContainPackage("netcat-openbsd"))
 	g.Expect(cfg).To(ContainPackage("nmap-netcat"))
 }
 
@@ -31,7 +31,6 @@ func TestPackageInstall(t *testing.T) {
 	cfg, g := NewFixture("packages.yml", t).WithFlags(DEBIAN).Build()
 	_, commands, _ := cfg.ApplyPhases()
 	g.Expect(commands).To(MatchCommand("apt-get install -y"))
-	g.Expect(commands).To(MatchCommand("docker-ce"))
 	g.Expect(commands).To(MatchCommand("socat"))
 	g.Expect(commands).To(MatchCommand("netcat"))
 	g.Expect(commands).NotTo(MatchCommand("yum"))
