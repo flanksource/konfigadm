0c69bc3de8394fe0f460a5dbf79874af2a426c81
Moshe Immerman <moshe.immerman@gmail.com>
2019-05-12 16:25:34 +0200

    add service test and verificaton

--- pkg/phases/services.go
+++ pkg/phases/services.go
@@ -1,8 +1,12 @@
 package phases
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
 
-import . "github.com/moshloop/configadm/pkg/types"
+	. "github.com/moshloop/configadm/pkg/types"
+	"github.com/moshloop/configadm/pkg/utils"
+)
 
 var Services Phase = services{}
 
@@ -14,9 +18,27 @@ func (p services) ApplyPhase(sys *Config, ctx *SystemContext) ([]Command, Filesy
 
 	for name, svc := range sys.Services {
 		filename := fmt.Sprintf("/etc/systemd/system/%s.service", name)
+		svc.Extra.Service.ExecStart = svc.ExecStart
+		svc.Extra.Unit.Description = name
 		files[filename] = File{Content: svc.Extra.ToUnitFile()}
 		commands = append(commands, Command{Cmd: "systemctl enable " + name})
 		commands = append(commands, Command{Cmd: "systemctl start " + name})
 	}
 	return commands, files, nil
+}
+
+func (p services) Verify(cfg *Config, results *VerifyResults, flags ...Flag) bool {
+	verify := true
+	for name, _ := range cfg.Services {
+		stdout, ok := utils.SafeExec("systemctl status %s | grep Active", name)
+		if !ok {
+			results.Fail("%s is not running %s", name, stdout)
+			verify = false
+		} else if strings.Contains(stdout, "active (running)") {
+			results.Pass("%s is  running %s", name, stdout)
+		} else {
+			results.Fail("%s is not running %s", name, stdout)
+		}
+	}
+	return verify
 }
