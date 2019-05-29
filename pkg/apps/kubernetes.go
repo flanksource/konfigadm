f1b88b7027b38882ad68b6e8ac49cd9aed25f016
Moshe Immerman <moshe.immerman@gmail.com>
2019-05-29 21:03:52 +0200

    fix kubernetres app

--- pkg/apps/kubernetes.go
+++ pkg/apps/kubernetes.go
@@ -16,7 +16,7 @@ func (k kubernetes) ApplyPhase(sys *Config, ctx *SystemContext) ([]Command, File
 			URL:             "https://apt.kubernetes.io/",
 			VersionCodeName: "kubernetes-xenial",
 			GPGKey:          "https://packages.cloud.google.com/apt/doc/apt-key.gpg",
-		}, DEBIAN).
+		}, DEBIAN_LIKE).
 		AppendPackageRepo(PackageRepo{
 			URL:    "https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64",
 			GPGKey: "https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg",
