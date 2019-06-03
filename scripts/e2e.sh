da004f3c62fafcd75d3a5ed9136f588e73b6a9c7
Moshe Immerman <moshe.immerman@gmail.com>
2019-06-03 17:31:26 +0200

    fix typo

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -14,6 +14,6 @@ cleanup() {
 docker cp $PWD $container:/$(dirname $PWD)
 trap cleanup EXIT
 mkdir -p test-output
-go test -v ./test -race -coverprofile=integ.txt -covermode=atomic $args | tee e2e.output
+go test -v ./test -race -coverprofile=integ.txt -covermode=atomic $args | tee e2e.out
 cat e2e.out | go2xunit -output test-output/$(date +%Y%m%d%M%H%M%S).xml
 rm e2e.out
