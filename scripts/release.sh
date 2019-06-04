0f381426f899fa264631cac6f537560d9a67677c
Moshe Immerman <moshe.immerman@gmail.com>
2019-06-04 09:58:13 +0200

    fix release

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,14 +3,16 @@ NAME=$(basename $(git remote get-url origin | sed 's/\.git//'))
 GITHUB_USER=$(basename $(dirname $(git remote get-url origin | sed 's/\.git//')))
 TAG=$(git tag --points-at HEAD )
 
-git stash
-git clean -fd
+
 if ! which goreleaser 2>&1 > /dev/null; then
-  // need to pin the version
+  # need to pin the version
   wget -nv https://github.com/goreleaser/goreleaser/releases/download/v0.108.0/goreleaser_amd64.deb
   sudo dpkg -i goreleaser_amd64.deb
-  goreleaser --rm-dist
 fi
+
+git stash
+git clean -fd
+goreleaser --rm-dist
 
 GO111MODULE=off go get github.com/aktau/github-release
 go get github.com/aktau/github-release
