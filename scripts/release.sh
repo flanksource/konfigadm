5edb85cf352d35bd521f6230ff6e853d0c39c419
Moshe Immerman <moshe.immerman@gmail.com>
2019-06-04 07:06:39 +0200

    fix release\

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-NAME=konfigadm
-GITHUB_USER=moshloop
+NAME=$(basename $(git remote get-url origin | sed 's/\.git//'))
+GITHUB_USER=$(basename $(dirname $(git remote get-url origin | sed 's/\.git//')))
 TAG=$(git tag --points-at HEAD )
 
-[[ -e "checkout.txt" ]] && rm checkout.txt
-go get github.com/goreleaser/goreleaser
-goreleaser release --rm-dist
+GO111MODULE=off go get github.com/goreleaser/goreleaser
+git stash
+git clean -fd
+docker run --rm --privileged -e GITHUB_TOKEN=$GITHUB_TOKEN -v $PWD:$PWD -v /var/run/docker.sock:/var/run/docker.sock -w $PWD goreleaser/goreleaser release --rm-dist
 
+GO111MODULE=off go get github.com/aktau/github-release
 go get github.com/aktau/github-release
 github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME} -f dist/linux_amd64/${NAME}
 github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME}_osx -f dist/darwin_amd64/${NAME}
