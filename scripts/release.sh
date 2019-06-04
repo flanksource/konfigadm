857dc826a9ada115561a051bbd3467eca7643db4
Moshe Immerman <moshe.immerman@gmail.com>
2019-06-04 09:45:27 +0200

    fix release

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,14 +3,16 @@ NAME=$(basename $(git remote get-url origin | sed 's/\.git//'))
 GITHUB_USER=$(basename $(dirname $(git remote get-url origin | sed 's/\.git//')))
 TAG=$(git tag --points-at HEAD )
 
-GO111MODULE=off go get github.com/goreleaser/goreleaser
 git stash
 git clean -fd
-docker run --rm --privileged -e GITHUB_TOKEN=$GITHUB_TOKEN -v $PWD:$PWD -v /var/run/docker.sock:/var/run/docker.sock -w $PWD goreleaser/goreleaser release --rm-dist
+if ! which goreleaser 2>&1 > /dev/null; then
+  // need to pin the version
+  wget -s https://github.com/goreleaser/goreleaser/releases/download/v0.108.0/goreleaser_amd64.deb
+  dpkg -i goreleaser_amd64.deb
+fi
 
 GO111MODULE=off go get github.com/aktau/github-release
 go get github.com/aktau/github-release
 github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME} -f dist/linux_amd64/${NAME}
 github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME}_osx -f dist/darwin_amd64/${NAME}
 github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME}.exe -f dist/windows_amd64/${NAME}.exe
-\
