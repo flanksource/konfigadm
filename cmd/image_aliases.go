9001dfc98c29787fc222360d86426a4583a860e0
Moshe Immerman <moshe.immerman@gmail.com>
2019-06-20 20:33:28 +0200

    fix fedora:29 url

--- cmd/image_aliases.go
+++ cmd/image_aliases.go
@@ -7,5 +7,5 @@ var images = map[string]string{
 	"centos:7":    "https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2",
 	"amazonLinux": "https://cdn.amazonlinux.com/os-images/2.0.20190612/kvm/amzn2-kvm-2.0.20190612-x86_64.xfs.gpt.qcow2",
 	"fedora:30":   "https://download.fedoraproject.org/pub/fedora/linux/releases/30/Cloud/x86_64/images/Fedora-Cloud-Base-30-1.2.x86_64.qcow2",
-	"fedora:29":   "http://fedora.is.co.za/linux/releases/29/Cloud/x86_64/images/Fedora-Cloud-Base-29-1.2.x86_64.qcow2",
+	"fedora:29":   "https://download.fedoraproject.org/pub/fedora/linux/releases/29/Cloud/x86_64/images/Fedora-Cloud-Base-29-1.2.x86_64.qcow2",
 }
