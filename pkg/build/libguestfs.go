e27a17489b68a43643772d92f2f7eca009ac7ed1
Moshe Immerman <moshe.immerman@gmail.com>
2019-06-20 09:21:55 +0200

    fix konfigadm not found error

--- pkg/build/libguestfs.go
+++ pkg/build/libguestfs.go
@@ -3,6 +3,7 @@ package build
 import (
 	"fmt"
 	"os"
+	"path"
 
 	"github.com/mitchellh/colorstring"
 	"github.com/moshloop/konfigadm/pkg/utils"
@@ -15,8 +16,14 @@ func (l Libguestfs) Build(image string, config *os.File) {
 	os.Remove("/tmp/builder.log")
 	os.Remove("builder.log")
 
-	konfigadmPath := os.Args[0]
-	cmdLine := fmt.Sprintf("virt-customize -a %s --delete /tmp/builder.log --copy-in %s:/tmp --copy-in %s:/tmp --run-command '/tmp/konfigadm apply -vv -c %s' ", image, konfigadmPath, config.Name(), config.Name())
+	konfigadmPath, _ := os.Executable()
+	konfigAdm := path.Base(konfigadmPath)
+	cmdLine := fmt.Sprintf(`
+		virt-customize -a %s \
+		--delete /tmp/builder.log \
+		--copy-in %s:/tmp \
+		--copy-in %s:/tmp \
+		--run-command '/tmp/%s apply -vv -c %s'`, image, konfigadmPath, config.Name(), konfigAdm, config.Name())
 
 	log.Infof("Executing %s\n", colorstring.Color("[light_green]"+cmdLine))
 	if err := utils.Exec(cmdLine); err != nil {
