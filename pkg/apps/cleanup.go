426d358e0479937c406277524e75f94c5e08b4f9
Moshe Immerman <moshe.immerman@gmail.com>
2019-06-26 18:14:51 +0200

    cleanup improvements

--- pkg/apps/cleanup.go
+++ pkg/apps/cleanup.go
@@ -22,25 +22,18 @@ func (c cleanup) ApplyPhase(sys *Config, ctx *SystemContext) ([]Command, Filesys
 	}
 
 	cmds.Add("rm -rf /tmp/* || true").
-		Add("rm -rf /var/log/cloud-init* || true").
 		Add("rm -rf /usr/share/man/* || true").
 		Add("rm -rf /usr/share/doc/* || true").
+		Add("rm /etc/udev/rules.d/70-persistent-net.rules || true").
 		Add("rm -f /etc/ssh/{ssh_host_dsa_key,ssh_host_dsa_key.pub,ssh_host_ecdsa_key,ssh_host_ecdsa_key.pub,ssh_host_ed25519_key,ssh_host_ed25519_key.pub,ssh_host_rsa_key,ssh_host_rsa_key.pub} || true").
 		Add("find /var/cache -type f -exec rm -rf {} \\;").
 		Add("find /var/log -type f | while read -r f; do echo -ne '' > \"$f\"; done;").
-		Add("cloud-init clean || true")
+		Add("sed -i '/^\\(HWADDR\\|UUID\\)=/d' /etc/sysconfig/network-scripts/ifcfg-* || true").
+		Add("rm -rf /var/lib/cloud/* || true").
+		Add("dd if=/dev/zero of=/EMPTY bs=1M  2>/dev/null || true;  rm -f /EMPTY")
 
-	fs["/root/.bash_profile"] = File{Content: "shopt -s histappend"}
 	fs["/etc/machine-id"] = File{Content: ""}
 	fs["/root/.bash_history"] = File{Content: ""}
-	fs["/root/.bashrc"] = File{Content: ""}
-
-	// TODO
-	// dd if=/dev/zero of=/EMPTY bs=1M  2>/dev/null || echo "dd exit code $? is suppressed"
-	// rm -f /EMPTY
-	//redhat
-	//sed -i '/^\(HWADDR\|UUID\)=/d' /etc/sysconfig/network-scripts/ifcfg-*
-	//debian /etc/udev/rules.d/70-persistent-net.rules
 
 	return cmds.GetCommands(), fs, nil
 }
