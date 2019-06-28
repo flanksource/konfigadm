e84b883306c42f1a5b70fdd02c6fdbb838d546cf
Moshe Immerman <moshe.immerman@gmail.com>
2019-06-28 08:27:34 +0200

    switch to libguqestfs params for qemu

--- pkg/build/qemu.go
+++ pkg/build/qemu.go
@@ -17,12 +17,27 @@ func (q Qemu) Build(image string, config *types.Config) {
 	cloud_init := config.ToCloudInit()
 	cloud_init.PowerState.Mode = "poweroff"
 	iso, _ := cloudinit.CreateISO("builder", cloud_init.String())
-	cmdLine := fmt.Sprintf(`
-	kvm -M pc -m 1024 -smp 2 \
-	-nographic \
-	-hda %s \
-	-drive "file=%s,if=virtio,format=raw" \
-	-net nic -net user,hostfwd=tcp:127.0.0.1:2022-:22`, image, iso)
+	cmdLine := fmt.Sprintf(`qemu-system-x86_64 \
+				-global virtio-blk-pci.scsi=off \
+				-enable-kvm \
+		    -enable-fips \
+		    -nodefaults \
+		    -display none \
+		    -machine accel=kvm \
+		    -cpu host -smp cpus=2 \
+		    -m 1024 \
+		    -no-reboot \
+		    -rtc driftfix=slew \
+		    -no-hpet \
+		    -global kvm-pit.lost_tick_policy=discard \
+		    -object rng-random,filename=/dev/urandom,id=rng0 \
+		    -device virtio-rng-pci,rng=rng0 \
+				-hda %s \
+		    -cdrom %s \
+		    -device virtio-serial-pci \
+		    -serial stdio \
+				-device sga \
+				-net nic -net user,hostfwd=tcp:127.0.0.1:2022-:22`, image, iso)
 
 	log.Infof("Executing %s\n", colorstring.Color("[light_green]"+cmdLine))
 	if err := utils.Exec(cmdLine); err != nil {
