70226bc14789d4f200770ff155dc4b5b4a2a69df
Moshe Immerman <moshe.immerman@gmail.com>
2019-07-04 19:39:21 +0200

    fix journal cleanup

--- pkg/apps/cleanup.go
+++ pkg/apps/cleanup.go
@@ -28,6 +28,7 @@ func (c cleanup) ApplyPhase(sys *Config, ctx *SystemContext) ([]Command, Filesys
 		Add("rm -f /etc/ssh/{ssh_host_dsa_key,ssh_host_dsa_key.pub,ssh_host_ecdsa_key,ssh_host_ecdsa_key.pub,ssh_host_ed25519_key,ssh_host_ed25519_key.pub,ssh_host_rsa_key,ssh_host_rsa_key.pub} || true").
 		Add("find /var/cache -type f -exec rm -rf {} \\;").
 		Add("find /var/log -type f | while read -r f; do echo -ne '' > \"$f\"; done;").
+		Add("journalctl --rotate && journalctl --vacuum-time=1s").
 		Add("sed -i '/^\\(HWADDR\\|UUID\\)=/d' /etc/sysconfig/network-scripts/ifcfg-* || true").
 		Add("rm -rf /var/lib/cloud/* || true").
 		Add("dd if=/dev/zero of=/EMPTY bs=1M  2>/dev/null || true;  rm -f /EMPTY")
