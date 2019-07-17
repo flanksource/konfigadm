981a332ee0f9eb88a61faa62eb4a64c5094c1bba
Moshe Immerman <moshe.immerman@gmail.com>
2019-07-17 19:45:42 +0200

    switch to ubuntu releases rather than nightly

--- cmd/image_aliases.go
+++ cmd/image_aliases.go
@@ -1,8 +1,8 @@
 package cmd
 
 var images = map[string]string{
-	"ubuntu:1804": "https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img",
-	"ubuntu:1604": "https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img",
+	"ubuntu:1804": "https://cloud-images.ubuntu.com/releases/18.04/release-20190617/ubuntu-18.04-server-cloudimg-amd64.img",
+	"ubuntu:1604": "https://cloud-images.ubuntu.com/releases/16.04/release-20190628/ubuntu-16.04-server-cloudimg-amd64-disk1.img",
 	"debian:9":    "https://cloud.debian.org/images/openstack/current-9/debian-9-openstack-amd64.qcow2",
 	"centos:7":    "https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2",
 	"amazonLinux": "https://cdn.amazonlinux.com/os-images/2.0.20190612/kvm/amzn2-kvm-2.0.20190612-x86_64.xfs.gpt.qcow2",
