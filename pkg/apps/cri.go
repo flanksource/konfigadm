6c0cc987d2579aae28bba633392b9a2b2427a965
Moshe Immerman <moshe.immerman@gmail.com>
2019-10-06 10:01:33 +0200

    fix debian codename detection

--- pkg/apps/cri.go
+++ pkg/apps/cri.go
@@ -113,7 +113,7 @@ func init() {
 			return version
 		}
 		id := "$(. /etc/os-release && echo $ID)"
-		codename := "$(. /etc/os-release && echo $VERSION_CODENAME)"
+		codename := "$(lsb_release -cs)"
 		if strings.Contains(version, "18.06") || strings.Contains(version, "18.03") {
 			return fmt.Sprintf("%s~ce~3-0~%s", version, id)
 		} else {
