72fc22781e3776a2edabf2129aea68a2ec666283
Moshe Immerman <moshe.immerman@gmail.com>
2019-10-06 14:52:41 +0200

    cleanup fixes

--- pkg/apps/cleanup.go
+++ pkg/apps/cleanup.go
@@ -31,15 +31,14 @@ func (c cleanup) ApplyPhase(sys *Config, ctx *SystemContext) ([]Command, Filesys
 		Add("rm -rf /tmp/* || true").
 		Add("rm -rf /usr/share/man/* || true").
 		Add("rm -rf /usr/share/doc/* || true").
+		Add("rm  /etc/netplan/50-cloud-init.yaml || true").
 		Add("rm /etc/udev/rules.d/70-persistent-net.rules || true").
 		Add("rm -f /etc/ssh/{ssh_host_dsa_key,ssh_host_dsa_key.pub,ssh_host_ecdsa_key,ssh_host_ecdsa_key.pub,ssh_host_ed25519_key,ssh_host_ed25519_key.pub,ssh_host_rsa_key,ssh_host_rsa_key.pub} || true").
+		Add("sed -i '/^\\(HWADDR\\|UUID\\)=/d' /etc/sysconfig/network-scripts/ifcfg-* || true").
 		Add("find /var/cache -type f -exec rm -rf {} \\;").
 		Add("find /var/log -type f | while read -r f; do echo -ne '' > \"$f\"; done;").
-		// cloud-init clean has some issues on 18.04 on subsequent boots
-		Add("rm /var/lib/cloud/sem/*").
-		Add("rm /var/lib/cloud/instance/sem/*").
-		Add("journalctl --rotate && journalctl --vacuum-time=1s").
-		Add("sed -i '/^\\(HWADDR\\|UUID\\)=/d' /etc/sysconfig/network-scripts/ifcfg-* || true").
+		Add("cloud-init clean").
+		Add("journalctl --rotate && sleep 5 && journalctl --vacuum-time=1s").
 		Add("echo Finished cleanup on $(date) > /var/log/cleanup.log").
 		Add("dd if=/dev/zero of=/EMPTY bs=1M  2>/dev/null || true;  rm -f /EMPTY")
 
