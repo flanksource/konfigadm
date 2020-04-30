692cf1fe9dbf11ca0b7a1642638d4b78fe919350
Moshe Immerman <moshe.immerman@gmail.com>
2020-04-30 21:10:19 +0200

    fix cleanup

--- pkg/apps/cleanup.go
+++ pkg/apps/cleanup.go
@@ -41,10 +41,10 @@ func (c cleanup) ApplyPhase(sys *Config, ctx *SystemContext) ([]Command, Filesys
 		Add("find /var/log -type f | while read -r f; do truncate -s 0 \"$f\"; done;").
 		Add("rm -rf /var/run/cloud-init || true").
 		Add("rm -rf /var/lib/cloud || true").
-		Add("journalctl --rotate && sleep 5 && journalctl --vacuum-time=1s").
-		Add("truncate -s 0 > /etc/machine-id").
+		Add("journalctl --rotate && sleep 5 && journalctl --vacuum-time=1s || true").
+		Add("truncate -s 0 /etc/machine-id").
 		Add("test -e /var/lib/dbus/machine-id  && truncate -s 0 /var/lib/dbus/machine-id").
-		Add("truncate -s 0 > /root/.bash_history").
+		Add("truncate -s 0 /root/.bash_history").
 		Add("echo Finished cleanup on $(date)  > /var/log/cleanup.log").
 		Add("dd if=/dev/zero of=/EMPTY bs=1M  2>/dev/null || true;  rm -f /EMPTY")
 
