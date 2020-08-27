445c597cacd37a8e8309a5e6c45c9723eff97f01
tochukwu <tochuwkwu.nwoko@gmail.com>
2020-08-27 23:32:41 +0100

    Added TestCopyUrl

--- pkg/phases/files_test.go
+++ pkg/phases/files_test.go
@@ -1,10 +1,12 @@
 package phases_test
 
 import (
+	"io/ioutil"
+	"net/http"
+	"testing"
+
 	. "github.com/flanksource/konfigadm/pkg/types"
 	"github.com/onsi/gomega"
-	"io/ioutil"
-	"testing"
 )
 
 func TestCopy(t *testing.T) {
@@ -13,6 +15,15 @@ func TestCopy(t *testing.T) {
 	data, _ := ioutil.ReadFile("../../fixtures/files.yml")
 	g.Expect(fs).To(gomega.HaveKey("/etc/test"))
 	g.Expect(fs["/etc/test"].Content).To(gomega.Equal(string(data)))
+}
+func TestCopyUrl(t *testing.T) {
+	cfg, g := NewFixture("files.yml", t).Build()
+	fs, _, _ := cfg.ApplyPhases()
+	g.Expect(fs).To(gomega.HaveKey("/etc/testurl"))
+	resp, _ := http.Get("https://www.geotrust.com/resources/root_certificates/certificates/GeoTrust_Primary_CA.pem")
+	defer resp.Body.Close()
+	data, _ := ioutil.ReadAll(resp.Body)
+	g.Expect(fs["/etc/testurl"].Content).To(gomega.Equal(string(data)))
 }
 
 func TestLookup(t *testing.T) {
