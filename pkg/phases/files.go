c6c8b9b665c6f31559b74d8ec5887d0a581c2264
tocy1 <nwokotochukwu@gmail.com>
2020-08-28 00:42:32 +0100

    Added LookupURL function for FilesPhase and driver code to AppyPhase

--- pkg/phases/files.go
+++ pkg/phases/files.go
@@ -1,10 +1,13 @@
 package phases
 
 import (
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+
 	"github.com/flanksource/konfigadm/pkg/types"
 	log "github.com/sirupsen/logrus"
-	"io/ioutil"
-	"os"
 )
 
 var Files types.Phase = filesPhase{}
@@ -19,6 +22,12 @@ func (p filesPhase) ApplyPhase(sys *types.Config, ctx *types.SystemContext) ([]t
 	}
 
 	for k, v := range sys.Files {
+		_, err := url.ParseRequestURI(v)
+		if err == nil {
+			files[k] = types.File{Content: LookupURL(v)}
+			continue
+
+		}
 		files[k] = types.File{Content: Lookup(v)}
 	}
 
@@ -49,6 +58,7 @@ func (p filesPhase) Verify(cfg *types.Config, results *types.VerifyResults, flag
 	return verify
 }
 
+//Lookup Function to Fetch content of file from path
 func Lookup(path string) string {
 	_, err := os.Stat(path)
 	if err != nil {
@@ -57,6 +67,30 @@ func Lookup(path string) string {
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Printf("Error reading: %s : %s", path, err)
+		return path
+	}
+	return string(data)
+}
+
+//LookupURL Function to Fetch content of file from url
+func LookupURL(path string) string {
+	_, err := url.ParseRequestURI(path)
+	if err != nil {
+		return path
+	}
+	resp, err := http.Get(path)
+	if err != nil {
+		log.Printf("GET Error %s : %s", path, err)
+		return path
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Status error: %v: %s", resp.StatusCode, path)
+		return path
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Read body: %s: %s", err, path)
 		return path
 	}
 	return string(data)
